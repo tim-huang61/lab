10efdf2217cf906ad6d9ce37ac4b7d7fbc56c6d9
tim <tim.huang61@gmail.com>
2019-03-16 11:53:14 +0800

    first version sequence equal

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -5,14 +5,47 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySequenceEqualTests
     {
         [Test]
-        public void compare_two_numbers_equal()
+        public void compare_two_numbers_equal1()
         {
-            var first = new List<int> { 3, 2, 1 };
-            var second = new List<int> { 3, 2, 1 };
+            var first = new List<int> {3, 2, 1};
+            var second = new List<int> {3, 2, 1};
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsTrue(actual);
+        }
+
+
+        [Test]
+        public void compare_two_numbers_equal2()
+        {
+            var first = new List<int> {3, 2, 1};
+            var second = new List<int> {1, 2, 3};
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal3()
+        {
+            var first = new List<int> {3, 2};
+            var second = new List<int> {3, 2, 1};
+
+            var actual = JoeySequenceEqual(first, second);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal4()
+        {
+            var first = new List<int> { };
+            var second = new List<int> { };
 
             var actual = JoeySequenceEqual(first, second);
 
@@ -21,7 +54,21 @@ namespace CSharpAdvanceDesignTests
 
         private bool JoeySequenceEqual(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+            while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
+            {
+                var firstEnumeratorCurrent = firstEnumerator.Current;
+                var secondEnumeratorCurrent = secondEnumerator.Current;
+                if (!(firstEnumerator.MoveNext() && firstEnumerator.MoveNext()))
+                {
+                    return false;
+                }
+                
+                return firstEnumeratorCurrent == secondEnumeratorCurrent;
+            }
+
+            return true;
         }
     }
 }
