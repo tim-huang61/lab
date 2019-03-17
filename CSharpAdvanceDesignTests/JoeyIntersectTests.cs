8b99829220f2342981394ee305b035fb6ab24382
tim <tim.huang61@gmail.com>
2019-03-17 10:39:01 +0800

    Intersect first version Tim

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,25 +5,38 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] {1, 3, 5, 3};
+            var second = new[] {5, 7, 3, 7};
 
             var actual = JoeyIntersect(first, second);
 
-            var expected = new[] { 3, 5 };
+            var expected = new[] {3, 5};
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var hashSet = new HashSet<int>();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+                var secondEnumerator = second.GetEnumerator();
+                while (secondEnumerator.MoveNext())
+                {
+                    var secondCurrent = secondEnumerator.Current;
+                    if (current == secondCurrent && hashSet.Add(current))
+                    {
+                        yield return current;
+                    }
+                }
+            }
         }
     }
 }
